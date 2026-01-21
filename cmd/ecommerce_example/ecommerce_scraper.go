3e0dd5527a4e2537a39039b3be82156ac7adeb34
Leon <leon.johansson@fasttrack.ai>
2026-01-21 10:18:15 +0100

    add: tablet scarping

--- cmd/ecommerce_example/ecommerce_scraper.go
+++ cmd/ecommerce_example/ecommerce_scraper.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main() {
-	scrapeUrl := "https://webscraper.io/test-sites"
+	scrapeUrl := "https://webscraper.io/test-sites/e-commerce/static/computers/tablets"
 
-	c = colly.NewCollector(colly.AllowedDomains("webscraper.io/test-sites"))
+	c := colly.NewCollector(colly.AllowedDomains("webscraper.io"))
 
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*",
@@ -21,10 +21,9 @@ func main() {
 		RandomDelay: 500 * time.Millisecond,
 	})
 
-	var tablet model.Tablet
+	var tablets []model.Tablet
 
 	c.OnRequest(func(r *colly.Request) {
-		// r.Headers.Set("Accept-Language", "en-US;q=0.9") 		// if webbsite supports multiple languages
 		fmt.Printf("Start scraping %s\n", r.URL)
 	})
 
@@ -32,5 +31,42 @@ func main() {
 		fmt.Printf("Something went wrong while scraping %s: %v\n", r.Request.URL, err)
 	})
 
-	c.OnHTML()
+	c.OnHTML(".thumbnail", func(e *colly.HTMLElement) {
+		tablet := model.Tablet{}
+
+		tablet.Name = e.ChildText("a.title")
+		fmt.Printf("Tablet found: %s\n", tablet.Name)
+
+		tablet.Price = e.ChildText("h4.price")
+		fmt.Printf("Tablet price: %s\n", tablet.Price)
+
+		tablet.Description = e.ChildText("p.description")
+		fmt.Printf("Tablet description: %s\n", tablet.Description)
+
+		// Collect all colors
+		e.ForEach("div.swatches > button[data-color]", func(_ int, el *colly.HTMLElement) {
+			color := el.Attr("data-color")
+			tablet.Color = append(tablet.Color, color)
+		})
+		fmt.Printf("Tablet colors: %v\n", tablet.Color)
+
+		// Collect all storage options
+		e.ForEach("div.swatches > button[data-storage]", func(_ int, el *colly.HTMLElement) {
+			storage := el.Attr("data-storage")
+			tablet.HardDrive = append(tablet.HardDrive, storage)
+		})
+		fmt.Printf("Tablet storage options: %v\n", tablet.HardDrive)
+
+		tablets = append(tablets, tablet)
+		fmt.Println("---")
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Printf("\nFinished scraping. Found %d tablets.\n", len(tablets))
+		for i, t := range tablets {
+			fmt.Printf("%d. %s - %s\n", i+1, t.Name, t.Price)
+		}
+	})
+
+	c.Visit(scrapeUrl)
 }
